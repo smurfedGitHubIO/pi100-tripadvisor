1ac2311f9af79e100687aee00d201ecdb2f0a68a
jcfrongoso <jvfrongoso@up.edu.ph>
2025-04-28 22:38:04 +0800

    chore: revert package.json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pi100-tripadvisor/",
   plugins: [svelte()],
-  build: {
-    outDir: 'docs',
-  }
 })
