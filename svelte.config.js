d8ad8fc5b80ac18e58c64ce267596980a8698d56
smurfedGitHubIO <13251277+smurfedGitHubIO@users.noreply.github.com>
2025-05-25 16:59:32 +0800

    chore: update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
+  kit: {
+    adapter: adapter()
+  },
   preprocess: vitePreprocess()
 }
